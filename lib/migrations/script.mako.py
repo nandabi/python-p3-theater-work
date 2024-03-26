95747339fe71b34275f20bc69b9d3ae55330125e
nandabi <valerie.nandabi@student.moringaschool.com>
2024-03-26 13:55:48 +0300

    commit message

--- lib/migrations/script.mako.py
+++ lib/migrations/script.mako.py
@@ -9,7 +9,7 @@ Create Date: ${create_date}
 from alembic import op
 import sqlalchemy as sa
 
-# revision identifiers, used by Alembic.
+
 revision = $ {repr(up_revision)}
 down_revision = $ {repr(down_revision)}
 branch_labels = $ {repr(branch_labels)}
